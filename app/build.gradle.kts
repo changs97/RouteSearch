462130f217c6ff9b68bbf1583fb3909abac25e39
changs97 <kin2027@gmail.com>
2024-06-09 23:29:51 +0900

    Add 네트워크 통신 라이브러리 종속성

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,15 @@ dependencies {
 
     // Timber
     implementation("com.jakewharton.timber:timber:5.0.1")
+
+    // Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    // Gson
+    implementation("com.google.code.gson:gson:2.10.1")
+
+    // Okhttp
+    implementation("com.squareup.okhttp3:okhttp-bom:4.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 }
