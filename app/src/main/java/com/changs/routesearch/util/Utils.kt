e196946f6fba4ddaa05c0cafcc4d6d166f9227cb
changs97 <kin2027@gmail.com>
2024-06-11 18:21:43 +0900

    Move safeFlow function from Utils.kt to ApiResult.kt

--- app/src/main/java/com/changs/routesearch/util/Utils.kt
+++ app/src/main/java/com/changs/routesearch/util/Utils.kt
@@ -1,22 +1,2 @@
 package com.changs.routesearch.util
 
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.flow
-import retrofit2.HttpException
-import retrofit2.Response
-
-fun <T> safeFlow(apiFunc: suspend () -> Response<T>): Flow<ApiResult<T>> = flow {
-    try {
-        val res = apiFunc.invoke()
-        if (res.isSuccessful) {
-            val body = res.body() ?: throw java.lang.NullPointerException()
-            emit(ApiResult.Success(body))
-        }
-    } catch (e: NullPointerException) {
-        emit(ApiResult.Empty)
-    } catch (e: HttpException) {
-        emit(ApiResult.Error(exception = e))
-    } catch (e: Exception) {
-        emit(ApiResult.Error(exception = e))
-    }
-}
